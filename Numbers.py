2b711c09d5add4fe85b07863dc2c23605e7717e6
George Kiragu <45111839+GeorMurk@users.noreply.github.com>
2019-02-01 20:01:05 +0300

    Add files via upload

---
+++ Numbers.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+#!Print whole numbers
+
+print(50)
+
+#!print Decimals
+
+print(50.2564)
+
+#!Arithemetics
+
+print(250 * 456)
+print(254 + 1546)
+print(15415 - 8964)
+
+#!complex math
+#!BODMAS
+print(25 * (56 + 48))
+#!MODULATOR (spit out the remainder)
+print(56 % 7)
+
+#!Convert a number into a string
+number_string = 89
+print(str(number_string))
+
+print(str(number_string) + "just a number")
+
+#!will give an error
+#!print(number_string + "Just a number")
+
+#!Absolute value
+num_exam = -45
+print(abs(num_exam))
+
+#!power or raised to the power of..
+numero = 8
+print(pow(numero, 3))
+
+#!print out the max number
+print(max(9, 5))
+#!print out the min number
+print(min(9, 5))
+#!round off a number
+print(round(79.4)) #!will give below .5 to the lower number
+print(round(79.7)) #!will give above .5 to the upper
+
+#!higher arithmetics you'll need to add an additional code
+from math import * #!allows us to do more math calculations
+
+print(floor(45.23)) #!Chop off after the decimal
+print(ceil(79.5)) #!round the number up no matter what
+print(sqrt(49)) #!find the square root
+
+
+#!Go ahead and test!!!!!
